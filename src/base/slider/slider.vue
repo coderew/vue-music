50afe2d41f909da811bb11772f6e87568a27eba1
coderew <1058478537@qq.com>
2017-11-24 22:02:55 +0800

    1

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="slider" ref="slider">
         <div class="slider-group" ref="sliderGroup">
-            <slot></slot>
+            <slot>
+
+            </slot>
         </div>
-        <div class="dots"></div>
+        <div class="dots">
+            <span class="dot" :class="{ active : currentPageIndex === index }" v-for="(item,index) in dots" ></span>
+        </div>
     </div>
 </template>
 
@@ -27,16 +31,44 @@
                 default: 4000
             }
         },
+        data() {
+            return {
+                dots: [],
+                currentPageIndex: 0
+            }
+        },
         mounted() {
             setTimeout(() => {
                 this._setSliderWidth()
+                this._initDots()
                 this._initSlider()
+                if (this.autoPlay) {
+                    this._play()
+                }
             }, 20)
+
+            window.addEventListener('resize', () => {
+                if (!this.slider) {
+                    return
+                }
+                this._setSliderWidth(true)
+                this.slider.refresh()
+            })
+        },
+        activated() {
+            if (this.autoPlay) {
+                this._play()
+            }
+        },
+        deactivated() {
+            clearTimeout(this.timer)
+        },
+        beforeDestroy() {
+            clearTimeout(this.timer)
         },
         methods: {
-            _setSliderWidth() {
+            _setSliderWidth(isResize) {
                 this.children = this.$refs.sliderGroup.children
-
                 let width = 0
                 let sliderWidth = this.$refs.slider.clientWidth
                 for (let i = 0; i < this.children.length; i++) {
@@ -45,7 +77,7 @@
                     child.style.width = sliderWidth + 'px'
                     width += sliderWidth
                 }
-                if (this.loop) {
+                if (this.loop && !isResize) {
                     width += 2 * sliderWidth
                 }
                 this.$refs.sliderGroup.style.width = width + 'px'
@@ -58,9 +90,33 @@
                     snap: true,
                     snapLoop: this.loop,
                     snapThreshold: 0.3,
-                    snapSpeed: 400,
-                    click: true
+                    snapSpeed: 400
                 })
+                this.slider.on('scrollEnd', () => {
+                    let pageIndex = this.slider.getCurrentPage().pageX
+                    // ???
+                    if (this.loop) {
+                        pageIndex -= 1
+                    }
+                    this.currentPageIndex = pageIndex
+
+                    if (this.autoPlay) {
+                        clearTimeout(this.timer)
+                        this._play()
+                    }
+                })
+            },
+            _initDots() {
+                this.dots = new Array(this.children.length)
+            },
+            _play() {
+                let pageIndex = this.currentPageIndex + 1
+                if (this.loop) {
+                    pageIndex += 1
+                }
+                this.timer = setTimeout(() => {
+                    this.slider.goToPage(pageIndex, 0, 400)
+                }, this.interval)
             }
         }
     }
