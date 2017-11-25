e1b3a9433dbea5111fb2867da4d7e85a4a2528be
coderew <1058478537@qq.com>
2017-11-25 14:20:50 +0800

    1

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -7,6 +7,9 @@
 
 <script type="text/ecmascript-6">
     import {mapGetters} from 'vuex'
+    import {getSingerDetail} from 'api/singer'
+    import {ERR_OK} from 'api/config'
+    import {createSong} from 'common/js/song'
 
     export default {
         computed: {
@@ -14,9 +17,39 @@
                 'singer'
             ])
         },
-        create() {
-            console.log(this.singer)
+        data() {
+            return {
+                songs: []
+            }
+        },
+        created() {
+            this._getDetail()
+        },
+        methods: {
+            _getDetail() {
+                if (!this.singer.id) {
+                    this.$router.push('/singer')
+                    return
+                }
+                getSingerDetail(this.singer.id).then((res) => {
+                    if (res.code === ERR_OK) {
+                        this.songs = this._normalizeSongs(res.data.list)
+                        console.log(this.songs)
+                    }
+                })
+            },
+            _normalizeSongs(list) {
+                let ret = []
+                list.forEach((item) => {
+                    let {musicData} = item
+                    if (musicData.songid && musicData.albummid) {
+                        ret.push(createSong(musicData))
+                    }
+                })
+                return ret
+            }
         }
+
     }
 </script>
 
@@ -24,17 +57,19 @@
     @import "~common/stylus/variable"
 
     .singer-detail
-        position: fixed;
-        z-index: 100;
-        top: 0;
-        left: 0;
+        position: fixed
+        z-index: 100
+        top: 0
+        left: 0
         right: 0
         bottom: 0
         background: $color-background
+
     // 待加强
     .slide-enter-active, .slide-leave-active
         transition: all 0.3s
-    .slider-enter, .slide-leave-to
+
+    .slide-enter, .slide-leave-to
         transform: translate3d(100%, 0, 0)
 
 </style>
