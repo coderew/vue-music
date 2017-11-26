25d5de805b3a8204115f2c1f97cb7f0548ceedc3
cdshilejian <cdshilejian@jd.com>
2017-11-26 11:03:28 +0800

    1

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,16 +1,26 @@
 <template>
     <div class="music-list">
-        <div class="back">
+        <div class="back" @click="back">
             <i class="icon-back"></i>
         </div>
         <h1 class="title" v-html="title"></h1>
         <div class="bg-image" :style="bgStyle" ref="bgImage">
-            <div class="filter"></div>
+            <div class="play-wrapper">
+                <div class="play" v-show="songs.length>0" ref="playBtn">
+                    <i class="icon-play"></i>
+                    <span class="text">随机播放全部</span>
+                </div>
+            </div>
+            <div class="filter" ref="filter"></div>
         </div>
         <div class="bg-layer" ref="layer"></div>
-        <scroll @scoll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
+        <scroll :data="songs" @scroll="scroll"
+                :listen-scroll="listenScroll" :probe-type="probeType" class="list" ref="list">
             <div class="song-list-wrapper">
                 <song-list :songs="songs"></song-list>
+            </div>
+            <div v-show="!songs.length" class="loading-container">
+                <loading></loading>
             </div>
         </scroll>
     </div>
@@ -19,6 +29,12 @@
 <script type="text/ecmascript-6">
     import Scroll from 'base/scroll/scroll'
     import SongList from 'base/song-list/song-list'
+    import Loading from 'base/loading/loading'
+    import {prefixStyle} from 'common/js/dom'
+
+    const RESERVED_HEIGHT = 40
+    const transform = prefixStyle('transform')
+    const backdrop = prefixStyle('backdrop-filter')
 
     export default {
         props: {
@@ -50,22 +66,57 @@
             this.listenScroll = true
         },
         mounted() {
+            this.imageHeight = this.$refs.bgImage.clientHeight
+            this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
             this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
         },
         methods: {
             scroll(pos) {
                 this.scrollY = pos.y
+            },
+            back() {
+                this.$router.back()
             }
         },
         watch: {
             scrollY(newY) {
-                this.$refs.layer.style['transform'] = `translate3d(0,${newY}px,0)`
-                this.$refs.layer.style['-webkit-transform'] = `translate3d(0,${newY}px,0)`
+                let translateY = Math.max(this.minTranslateY, newY)
+                let zIndex = 0
+                let scale = 1
+                let blur = 0
+
+                const percent = Math.abs(newY / this.imageHeight)
+                if (newY > 0) {
+                    // 图片的高度乘以sale,相当于增加了newY
+                    scale = 1 + percent
+                    zIndex = 10
+                } else {
+                    // 向上滑
+                    blur = Math.min(20 * percent, 20)
+                }
+                // 电脑上和Android看不出来该效果
+                this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+                this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+                if (newY < this.minTranslateY) {
+                    zIndex = 10
+                    // css样式中得到了宽高比为10:7,如果设置高度，需要将paddingTop设置为0
+                    this.$refs.bgImage.style.paddingTop = 0
+                    this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+                    this.$refs.playBtn.style.display = 'none'
+                } else {
+                    // 下拉
+                    this.$refs.bgImage.style.paddingTop = '70%'
+                    this.$refs.bgImage.style.height = 0
+                    this.$refs.playBtn.style.display = ''
+                }
+                this.$refs.bgImage.style[transform] = `scale(${scale})`
+                this.$refs.bgImage.style.zIndex = zIndex
             }
         },
         components: {
             Scroll,
-            SongList
+            SongList,
+            Loading
         }
     }
 </script>
