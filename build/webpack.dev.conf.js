8b5c0b737827b887d248badf72bdf5d30996a015
cdshilejian <cdshilejian@jd.com>
2017-12-09 15:13:41 +0800

    1.功能开发完成

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -67,7 +67,7 @@ var proxyTable = config.dev.proxyTable;
 Object.keys(proxyTable).forEach(function (context) {
     var options = proxyTable[context]
     if (typeof options === 'string') {
-        options = { target: options }
+        options = {target: options}
     }
     app.use(proxy(options.filter || context, options))
 })
@@ -112,28 +112,28 @@ const devWebpackConfig = merge(baseWebpackConfig, {
                     console.log(e)
                 })
             }),
-            app.get('/lyric', function (req, res) {
-                var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
-                axios.get(url, {
-                    headers: {
-                        referer: 'https://c.y.qq.com/',
-                        host: 'c.y.qq.com'
-                    },
-                    params: req.query
-                }).then((response) => {
-                    var ret = response.data
-                    if (typeof ret === 'string') {
-                        var reg = /^\w+\(({[^()]+})\)$/
-                        var matches = ret.match(reg)
-                        if (matches) {
-                            ret = JSON.parse(matches[1])
+                app.get('/lyric', function (req, res) {
+                    var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+                    axios.get(url, {
+                        headers: {
+                            referer: 'https://c.y.qq.com/',
+                            host: 'c.y.qq.com'
+                        },
+                        params: req.query
+                    }).then((response) => {
+                        var ret = response.data
+                        if (typeof ret === 'string') {
+                            var reg = /^\w+\(({[^()]+})\)$/
+                            var matches = ret.match(reg)
+                            if (matches) {
+                                ret = JSON.parse(matches[1])
+                            }
                         }
-                    }
-                    res.json(ret)
-                }).catch((e) => {
-                    console.log(e)
+                        res.json(ret)
+                    }).catch((e) => {
+                        console.log(e)
+                    })
                 })
-            })
         }
     },
     plugins: [
